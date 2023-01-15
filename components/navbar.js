7d334f1682f002cb1911af55f512441e425cbbc1
sarahppp <sarahpan888@gmail.com>
2023-01-14 23:20:38 -0500

    remove contact page from navbar

--- components/navbar.js
+++ components/navbar.js
@@ -15,11 +15,11 @@ export default function Navbar(props) {
     {
       label: "About",
       href: "/about"
-    },
-    {
-      label: "Contact",
-      href: "/contact"
     }
+    // {
+    //   label: "Contact",
+    //   href: "/contact"
+    // }
   ];
 
   const rightmenu = [
